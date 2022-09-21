51dd2207313611bc676120bc958d75bb657c84d5
Mohamed.ABDC <41598266+mohamedabcd@users.noreply.github.com>
2022-09-21 14:16:42 +0200

    Create powershell.ps1

---
+++ powershell.ps1
@@ -0,0 +1,15 @@
+# New-Item .\ReadMe.md
+$file = ".\ReadMe.md"
+if (Test-Path $file) 
+{
+  Remove-Item $file
+}
+# $file = ".\ReadMe.md"
+New-Item $file -ItemType File -Value
+Add-Content $file "# DockerFactory"
+Add-Content $file ""
+Add-Content $file "|image|tags|url|"
+Add-Content $file "|--|--|--|"
+Add-Content $file "|a/cli|latest-stable b-test|"
+Add-Content $file "|a/cli|latest-stable b-test|"
+
